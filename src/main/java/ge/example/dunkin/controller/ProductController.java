ffa475801f57c4ad5ce0ed650e14a93c2a714c07
tatia <tatia.gogashvili708@ens.tsu.edu.ge>
2022-02-26 13:43:16 +0400

    sgz

--- src/main/java/ge/example/dunkin/controller/ProductController.java
+++ src/main/java/ge/example/dunkin/controller/ProductController.java
@@ -3,15 +3,14 @@ package ge.example.dunkin.controller;
 import ge.example.dunkin.model.Product;
 import ge.example.dunkin.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("product")
+@CrossOrigin("http://localhost:3000/")
 public class ProductController {
     ProductService productService;
 
@@ -20,6 +19,15 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @GetMapping("all")
+    public List<Product> allProd(){
+        List<Product> prod=new ArrayList<>();
+        Product product=new Product();
+        product.setId(1);
+        product.setSinglePrise(15.2);
+        prod.add(product);
+        return prod;
+    }
 
     @GetMapping
     public List<Product> filter(@RequestParam(required = false) Integer typeId, @RequestParam(required = false) Integer quantity, @RequestParam(required = false) Double minSinglePrice, @RequestParam(required = false) Double maxSinglePrice) {
